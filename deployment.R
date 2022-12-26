707cf783e860a8e2808b019b1efc55272a4b539c
moesslerm <53613876+mmoessler@users.noreply.github.com>
2022-12-26 21:32:46 +0100

    Added script for deployment on POSIT

---
+++ deployment.R
@@ -0,0 +1,11 @@
+
+getwd()
+
+setwd("C:/Users/Markus/Dropbox/Implementations/GitHub/Bookdown/juselius-2006/_book")
+
+# In a directory with app.R
+list.files()
+
+rsconnect::writeManifest(contentCategory = "site")
+
+list.files()
