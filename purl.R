78610f22fc06445308b0bdce0b846e262a10e9d4
mmoessler <markus.moessler@gmail.com>
2023-01-03 20:00:27 +0100

    Added purl for 03-cointegrated-var.R

--- purl.R
+++ purl.R
@@ -1,5 +1,10 @@
 
 library(knitr)
+
 knitr::purl(input = "01-data.Rmd",
             output = "./R/01-data.R",
+            documentation = 0)
+
+knitr::purl(input = "03-cointegrated-var.Rmd",
+            output = "./R/03-cointegrated-var.R",
             documentation = 0)
